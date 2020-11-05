5175f8f6e73eac8aa681290828ddf6d2bef0733c
Zhaomin777888 <fanmn163@163.com>
2020-11-05 10:47:16 +0800

    update 1

--- src/main/java/com/atguigu/guangmingding/HanBingZhang.java
+++ src/main/java/com/atguigu/guangmingding/HanBingZhang.java
@@ -3,5 +3,6 @@ package com.atguigu.guangmingding;
 public class HanBingZhang {
     public static void main(String[] args) {
         System.out.println("hanbingzhang");
+        System.out.println("一阳指");
     }
 }
