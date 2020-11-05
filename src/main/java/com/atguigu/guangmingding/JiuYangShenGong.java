55914f92d8de672e724b9cbc62b98f52d1b41afc
Zhaomin777888 <fanmn163@163.com>
2020-11-05 10:05:29 +0800

    update 2

--- src/main/java/com/atguigu/guangmingding/JiuYangShenGong.java
+++ src/main/java/com/atguigu/guangmingding/JiuYangShenGong.java
@@ -3,5 +3,6 @@ package com.atguigu.guangmingding;
 public class JiuYangShenGong {
     public static void main(String[] args) {
         System.out.println("jiuyangshengong");
+        System.out.println("jiuyinbaiguzhua");
     }
 }
